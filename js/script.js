a801b88c11ff35edadc0293b300c0c305b3915e2
Mohd Rejoan <wearecrx@gmail.com>
2021-12-16 14:03:21 +0600

    make first two item selectable at a time version 2.0.1

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const activeImg = [
     "./icons/active/wedding-ring.png",
 ];
 const image = [
-  "./icons/wedding.png",
+    "./icons/wedding.png",
     "./icons/diamond.png",
     "./icons/wedding-ring.png",
 ];
@@ -22,5 +22,22 @@ breadItem.forEach((item, index) => {
         removeItem();
         item.classList.add("active");
         item.querySelector(".img img").src = activeImg[index];
+        markAll();
     };
 });
+
+function markAll() {
+    if (
+        document
+            .querySelector(".breadcrumb .bread-item:nth-child(2)")
+            .classList.contains("active")
+    ) {
+        const firstItem = document.querySelector(
+            ".breadcrumb .bread-item:nth-child(1)"
+        );
+        firstItem.classList.add("active");
+        firstItem.style.zIndex = "15";
+        firstItem.querySelector(".img img").src = activeImg[0];
+    }
+}
+
